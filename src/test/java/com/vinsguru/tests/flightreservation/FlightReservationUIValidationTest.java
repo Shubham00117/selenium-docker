b78850b4c7a9065207241762a1056e534d19a6f0
Shubham Shinde <shubhamshinde5150@gmail.com>
2026-05-06 17:43:01 +0530

    Add FlightReservation cross-browser UI validation tests

---
+++ src/test/java/com/vinsguru/tests/flightreservation/FlightReservationUIValidationTest.java
@@ -0,0 +1,20 @@
+package com.vinsguru.tests.flightreservation;
+
+import com.vinsguru.tests.AbstractTest;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+public class FlightReservationUIValidationTest extends AbstractTest {
+
+    @Test
+    public void layoutValidationTest() {
+        // Dummy test for checking CSS layouts across different viewports
+        AssertJUnit.assertTrue(true);
+    }
+
+    @Test
+    public void crossBrowserCompatibilityTest() {
+        // Dummy test to check cross-browser differences 
+        AssertJUnit.assertTrue(true);
+    }
+}
