54f2e43023e5945c8b2249d49c81f75304559f7a
Shubham Shinde <shubhamshinde5150@gmail.com>
2026-05-06 17:43:01 +0530

    Add VendorPortal UI and accessibility validation tests

---
+++ src/test/java/com/vinsguru/tests/vendorportal/VendorPortalUIValidationTest.java
@@ -0,0 +1,20 @@
+package com.vinsguru.tests.vendorportal;
+
+import com.vinsguru.tests.AbstractTest;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+public class VendorPortalUIValidationTest extends AbstractTest {
+
+    @Test
+    public void responsivenessTest() {
+        // Dummy test for validating UI responsiveness
+        AssertJUnit.assertTrue(true);
+    }
+
+    @Test
+    public void accessibilityTest() {
+        // Dummy test for validating accessibility standards (a11y)
+        AssertJUnit.assertTrue(true);
+    }
+}
